9f3a2d8ef5d3dd7420f8e93d7036faba7f983267
GVshare <gregory.vigliola@gmail.com>
2019-03-13 00:12:39 +0100

    updated the postView witht the comments taken from the DB

--- view/postView.php
+++ view/postView.php
@@ -3,7 +3,7 @@
 <h1>Mon super blog ! Ou pas...</h1>
 <p><a href="index.php">Retour à la liste des billets</a></p>
 
-<div class="news">
+<div class="post">
     <h3>
         <?= htmlspecialchars($post['title']) ?>
         <em>le <?= $post['commentDateFr'] ?></em>
@@ -12,6 +12,21 @@
     <p>
         <?= nl2br(htmlspecialchars($post['content'])) ?>
     </p>
+</div>
+
+<div class="comments">
+
+	<h3>Commentaires : </h3>
+
+	<?php
+	while ($comment = $comments->fetch())
+	{
+	?>
+	    <p><strong><?= htmlspecialchars($comment['author']) ?> le <?= $comment['commentDateFr'] ?></strong></p>
+	    <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+	<?php
+	}
+	?>
 </div>
 
 <?php $content = ob_get_clean(); ?>
